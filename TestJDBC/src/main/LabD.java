f1d58097d5fc58fb0a7f461fb2520d6c33629388
anzhehong <anzhehong@126.com>
2015-11-14 10:14:53 +0800

    jdbc class 2

--- TestJDBC/src/main/LabD.java
+++ TestJDBC/src/main/LabD.java
@@ -4,52 +4,49 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Set;
 
-import com.mysql.jdbc.UpdatableResultSet;
 
-import jdk.nashorn.internal.objects.annotations.Where;
+import java.sql.PreparedStatement;
 
-public class LabB {
+public class LabD {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 		String DB_URL = "jdbc:mysql://10.60.42.66/stu_1352834";
 		
-//		String USER_NAME = "1352834";
-//		String USER_PASSWORD = "4ccstu5x";
 		String USER_NAME = "1352834";
 		String USER_PASSWORD = "4ccstu5x";
 		
 		Connection conn = null;
+		PreparedStatement pstmt = null;
 		
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn = DriverManager.getConnection(DB_URL, USER_NAME, USER_PASSWORD);
-			Statement stmt = conn.createStatement();
 			if (conn != null) {
 				System.out.println("ok");
-				
-//				String sql = "UPDATE t_fruit Set country = 'USA' Where country = 'china'"; 
-//				int i = stmt.executeUpdate(sql);
 			}
+			String sql = "UPDATE t_fruit set price = ? where country = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setFloat(1, 110);
+			pstmt.setString(2, "USA");
+			pstmt.executeUpdate();
+			
+//			pstmt.setFloat(1, 20);
+//			pstmt.setString(2, "USaA");
+//			pstmt.executeUpdate();
+			
 		} catch (ClassNotFoundException e) {
-			// : handle exception
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO: handle exception
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
-//				stmt.close();
+				pstmt.close();
 				conn.close();
 			} catch (SQLException e2) {
 				// TODO: handle exception
-				e2.printStackTrace();
 			}
 		}
-		
 		
 	}
 
--- TestJDBC/src/main/LabE.java
+++ TestJDBC/src/main/LabE.java
@@ -1,55 +1,50 @@
 package main;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Set;
 
-import com.mysql.jdbc.UpdatableResultSet;
-
-import jdk.nashorn.internal.objects.annotations.Where;
-
-public class LabB {
+public class LabE {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 		String DB_URL = "jdbc:mysql://10.60.42.66/stu_1352834";
 		
-//		String USER_NAME = "1352834";
-//		String USER_PASSWORD = "4ccstu5x";
 		String USER_NAME = "1352834";
 		String USER_PASSWORD = "4ccstu5x";
 		
 		Connection conn = null;
+		CallableStatement cstmt = null;
 		
 		try {
 			Class.forName(JDBC_DRIVER);
 			conn = DriverManager.getConnection(DB_URL, USER_NAME, USER_PASSWORD);
-			Statement stmt = conn.createStatement();
 			if (conn != null) {
 				System.out.println("ok");
-				
-//				String sql = "UPDATE t_fruit Set country = 'USA' Where country = 'china'"; 
-//				int i = stmt.executeUpdate(sql);
 			}
+			String sql = "{call count_fruit_per_country(?,?)}";
+
+			cstmt = conn.prepareCall(sql);
+			cstmt.setString(1, "USA");
+			cstmt.registerOutParameter(2, java.sql.Types.INTEGER);
+			cstmt.execute();
+			
+			System.out.println(cstmt.getInt(2));
+			
+			
 		} catch (ClassNotFoundException e) {
-			// : handle exception
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO: handle exception
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
-//				stmt.close();
+				cstmt.close();
 				conn.close();
 			} catch (SQLException e2) {
 				// TODO: handle exception
-				e2.printStackTrace();
 			}
 		}
-		
 		
 	}
 
